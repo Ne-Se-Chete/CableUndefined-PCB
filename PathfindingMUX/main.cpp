9842bb0aa30bbf9faa83d8e61e17e0fc4db3a30f
Yasen Tsvetkov <yasentsv@gmail.com>
2024-01-29 19:23:29 +0200

    init main multiplexer code classes

--- PathfindingMUX/main.cpp
+++ PathfindingMUX/main.cpp
@@ -1,28 +1,76 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+
 using namespace std;
 
-typedef struct Multiplexer {
-    int num;  // Identifier for the multiplexer
-    struct Node* x[16];
-    struct Node* y[8];
-    
-    Multiplexer(int n) : num(n) {
+class Multiplexer;  // Forward declaration
+
+class Node {
+public:
+    Multiplexer* mux;
+    int index;
+    char connectionType;
+
+    Node(Multiplexer* m, int i, char type);
+
+    void print() const;
+};
+
+class Multiplexer {
+    public:
+        int num;
+        Node* x[16];
+        Node* y[8];
+
+        Multiplexer(int n);
+
+        void printConnections() const;
+    };
+
+    Node::Node(Multiplexer* m, int i, char type) : mux(m), index(i), connectionType(type) {}
+
+    void Node::print() const {
+        cout << "MUX_" << mux->num << "." << connectionType << "[" << index << "]";
+    }
+
+    Multiplexer::Multiplexer(int n) : num(n) {
         for (auto& xi : x) xi = nullptr;
         for (auto& yi : y) yi = nullptr;
     }
-} Multiplexer;
 
-struct Node {
-    Multiplexer* mux;
-    int index;
-    bool isX;
+    void Multiplexer::printConnections() const {
+        for (int i = 0; i < 16; ++i) {
+            if (x[i]) {
+                cout << "MUX_" << num << ".x[" << i << "] -> ";
+                x[i]->print();
+                cout << endl;
+            }
+        }
+        for (int i = 0; i < 8; ++i) {
+            if (y[i]) {
+                cout << "MUX_" << num << ".y[" << i << "] -> ";
+                y[i]->print();
+                cout << endl;
+            }
+        }
+}
 
-    Node(Multiplexer* m, int i, bool x) : mux(m), index(i), isX(x) {}
+int main() {
+    Multiplexer mux1(1), mux2(2), mux3(3);
 
-    // Helper function to print node information
-    void print() {
-        cout << "MUX" << mux->num << (isX ? ".x[" : ".y[") << index << "]";
-    }
-};
+    mux3.x[1] = new Node(&mux2, 3, 'x');
+    mux1.y[2] = new Node(&mux2, 4, 'y');
+
+    // Print connections
+    mux1.printConnections();
+    cout << "-------------------"<< endl;
+    mux3.printConnections();
+
+
+    // Clean up memory (deallocate Nodes)
+    // delete mux1.x[1];
+    // delete mux1.y[2];
+
+    return 0;
+}
