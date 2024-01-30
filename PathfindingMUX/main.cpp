8db06eb2bdcf28a9d5055de9c8bd301299aaf11a
Yasen Tsvetkov <yasentsv@gmail.com>
2024-01-30 21:28:05 +0200

    baca

--- PathfindingMUX/main.cpp
+++ PathfindingMUX/main.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 class Multiplexer;
 
-class Node {
+class ConnectionNode {
 public:
     Multiplexer* mux;
     int index;
     char connectionType;
 
-    Node(Multiplexer* m, int i, char type);
+    ConnectionNode(Multiplexer* m, int i, char type);
 
     void print() const;
 };
@@ -20,17 +20,17 @@ public:
 class Multiplexer {
 public:
     int num;
-    Node* x[16];
-    Node* y[8];
+    ConnectionNode* x[16];
+    ConnectionNode* y[8];
 
     Multiplexer(int n);
 
     void printConnections() const;
 };
 
-Node::Node(Multiplexer* m, int i, char type) : mux(m), index(i), connectionType(type) {}
+ConnectionNode::ConnectionNode(Multiplexer* m, int i, char type) : mux(m), index(i), connectionType(type) {}
 
-void Node::print() const {
+void ConnectionNode::print() const {
     cout << "MUX_" << mux->num << "." << connectionType << "[" << index << "]";
 }
 
@@ -57,8 +57,8 @@ void Multiplexer::printConnections() const {
 }
 
 bool checkBidirectionalConnection(Multiplexer& mux1, char type1, int index1, Multiplexer& mux2, char type2, int index2) {
-    Node* node1 = (type1 == 'x') ? mux1.x[index1] : mux1.y[index1];
-    Node* node2 = (type2 == 'x') ? mux2.x[index2] : mux2.y[index2];
+    ConnectionNode* node1 = (type1 == 'x') ? mux1.x[index1] : mux1.y[index1];
+    ConnectionNode* node2 = (type2 == 'x') ? mux2.x[index2] : mux2.y[index2];
 
     if (node1 && node2 && node1->mux == &mux2 && node2->mux == &mux1 && node1->index == index2 && node2->index == index1) {
         return true;
@@ -69,13 +69,19 @@ bool checkBidirectionalConnection(Multiplexer& mux1, char type1, int index1, Mul
 int main() {
     Multiplexer mux1(1), mux2(2), mux3(3);
 
-    mux3.x[1] = new Node(&mux2, 3, 'x');
-    mux1.y[2] = new Node(&mux2, 4, 'y');
+    mux3.x[1] = new ConnectionNode(&mux2, 3, 'x');
+    mux1.y[2] = new ConnectionNode(&mux2, 4, 'y');
 
-    mux2.x[3] = new Node(&mux3, 1, 'x');
-    mux2.y[4] = new Node(&mux1, 2, 'y');
+    mux2.x[3] = new ConnectionNode(&mux3, 1, 'x');
+    mux2.y[4] = new ConnectionNode(&mux1, 2, 'y');
 
-    // Check bidirectional connections
+    mux1.printConnections();
+    cout << "\n"; 
+    mux2.printConnections();
+    cout << "\n";
+    mux3.printConnections();
+    cout << "\n";
+
     if (checkBidirectionalConnection(mux3, 'x', 1, mux2, 'x', 3)) {
         cout << "Bidirectional connection between MUX3.x[1] and MUX2.x[3]" << endl;
     } else {
@@ -88,11 +94,6 @@ int main() {
         cout << "No bidirectional connection between MUX1.y[2] and MUX2.y[4]" << endl;
     }
 
-    // Clean up memory (deallocate Nodes)
-    // delete mux3.x[1];
-    // delete mux1.y[2];
-    // delete mux2.x[3];
-    // delete mux2.y[4];
 
     return 0;
 }
