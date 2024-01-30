8fe1c5ee576a6da2cb196c71cd3433766aa66e2a
Yasen Tsvetkov <yasentsv@gmail.com>
2024-01-30 22:16:01 +0200

    add breadboard

--- PathfindingMUX/main.cpp
+++ PathfindingMUX/main.cpp
@@ -4,39 +4,63 @@
 
 using namespace std;
 
-class Multiplexer;
-
-class ConnectionNode {
+class Device {
 public:
-    Multiplexer* mux;
+    int num;
+
+    Device(int n) : num(n) {}
+
+    virtual void printConnections() const = 0;
+};
+
+class ConnectionNode{
+public:
+    Device* device;
     int index;
     char connectionType;
 
-    ConnectionNode(Multiplexer* m, int i, char type);
+    ConnectionNode(Device* d, int i, char type);
 
     void print() const;
 };
 
-class Multiplexer {
+class Multiplexer : public Device {
 public:
-    int num;
     ConnectionNode* x[16];
     ConnectionNode* y[8];
 
     Multiplexer(int n);
 
-    void printConnections() const;
+    void printConnections() const override;
 };
 
-ConnectionNode::ConnectionNode(Multiplexer* m, int i, char type) : mux(m), index(i), connectionType(type) {}
+// Derived class for a breadboard device
+class Breadboard : public Device {
+public:
+    ConnectionNode* pin[64];
+
+    Breadboard(int n);
+
+    void printConnections() const override;
+};
+
+ConnectionNode::ConnectionNode(Device* d, int i, char type) : device(d), index(i), connectionType(type) {}
 
 void ConnectionNode::print() const {
-    cout << "MUX_" << mux->num << "." << connectionType << "[" << index << "]";
+    if (dynamic_cast<Multiplexer*>(device)) {
+        cout << "MUX_" << device->num << "." << connectionType << "[" << index << "]";
+    } else if (dynamic_cast<Breadboard*>(device)) {
+        cout << "Breadboard_" << device->num << ".pin[" << index << "]";
+    }
 }
 
-Multiplexer::Multiplexer(int n) : num(n) {
+Multiplexer::Multiplexer(int n) : Device(n) {
     for (auto& xi : x) xi = nullptr;
     for (auto& yi : y) yi = nullptr;
+}
+
+Breadboard::Breadboard(int n) : Device(n) {
+    for (auto& pin : pin) pin = nullptr;
 }
 
 void Multiplexer::printConnections() const {
@@ -56,43 +80,83 @@ void Multiplexer::printConnections() const {
     }
 }
 
-bool checkBidirectionalConnection(Multiplexer& mux1, char type1, int index1, Multiplexer& mux2, char type2, int index2) {
-    ConnectionNode* node1 = (type1 == 'x') ? mux1.x[index1] : mux1.y[index1];
-    ConnectionNode* node2 = (type2 == 'x') ? mux2.x[index2] : mux2.y[index2];
-
-    if (node1 && node2 && node1->mux == &mux2 && node2->mux == &mux1 && node1->index == index2 && node2->index == index1) {
-        return true;
+void Breadboard::printConnections() const {
+    for (int i = 0; i < 64; ++i) {
+        if (pin[i]) {
+            cout << "Breadboard_" << num << ".pin[" << i << "] -> ";
+            pin[i]->print();  // This will print either MUX or Breadboard connection information
+            cout << endl;
+        }
     }
+}
+
+
+bool checkBidirectionalConnection(Device& device1, char type1, int index1, Device& device2, char type2, int index2) {
+    if (auto mux1 = dynamic_cast<Multiplexer*>(&device1)) {
+        if (auto mux2 = dynamic_cast<Multiplexer*>(&device2)) {
+            ConnectionNode* node1 = (type1 == 'x') ? mux1->x[index1] : mux1->y[index1];
+            ConnectionNode* node2 = (type2 == 'x') ? mux2->x[index2] : mux2->y[index2];
+
+            if (node1 && node2 && node1->device == &device2 && node2->device == &device1 && node1->index == index2 && node2->index == index1) {
+                return true;
+            }
+        } else if (auto breadboard2 = dynamic_cast<Breadboard*>(&device2)) {
+            ConnectionNode* node1 = (type1 == 'x') ? mux1->x[index1] : mux1->y[index1];
+            ConnectionNode* node2 = breadboard2->pin[index2];
+
+            if (node1 && node2 && node1->device == &device2 && node2->device == &device1 && node1->index == index2 && node2->index == index1) {
+                return true;
+            }
+        }
+    } else if (auto breadboard1 = dynamic_cast<Breadboard*>(&device1)) {
+        if (auto mux2 = dynamic_cast<Multiplexer*>(&device2)) {
+            ConnectionNode* node1 = breadboard1->pin[index1];
+            ConnectionNode* node2 = (type2 == 'x') ? mux2->x[index2] : mux2->y[index2];
+
+            if (node1 && node2 && node1->device == &device2 && node2->device == &device1 && node1->index == index2 && node2->index == index1) {
+                return true;
+            }
+        }
+    }
+
     return false;
 }
 
 int main() {
     Multiplexer mux1(1), mux2(2), mux3(3);
+    Breadboard breadboard1(4), breadboard2(5);
 
-    mux3.x[1] = new ConnectionNode(&mux2, 3, 'x');
-    mux1.y[2] = new ConnectionNode(&mux2, 4, 'y');
+    mux1.y[0] = new ConnectionNode(&breadboard1, 10, 'y');
+    mux2.x[5] = new ConnectionNode(&breadboard1, 20, 'x');
+    mux3.y[7] = new ConnectionNode(&breadboard2, 15, 'y'); 
 
-    mux2.x[3] = new ConnectionNode(&mux3, 1, 'x');
-    mux2.y[4] = new ConnectionNode(&mux1, 2, 'y');
+    breadboard1.pin[10] = new ConnectionNode(&mux1, 0, 'y');
+    breadboard1.pin[20] = new ConnectionNode(&mux2, 5, 'x');
+    breadboard2.pin[15] = new ConnectionNode(&mux3, 7, 'y');
 
+
+    cout << "MUX1 connections:\n";
     mux1.printConnections();
-    cout << "\n"; 
+    cout << "\n";
+
+    cout << "MUX2 connections:\n";
     mux2.printConnections();
     cout << "\n";
+
+    cout << "MUX3 connections:\n";
     mux3.printConnections();
     cout << "\n";
 
-    if (checkBidirectionalConnection(mux3, 'x', 1, mux2, 'x', 3)) {
-        cout << "Bidirectional connection between MUX3.x[1] and MUX2.x[3]" << endl;
-    } else {
-        cout << "No bidirectional connection between MUX3.x[1] and MUX2.x[3]" << endl;
-    }
+    cout << "Breadboard1 connections:\n";
+    breadboard1.printConnections();
+    cout << "\n";
 
-    if (checkBidirectionalConnection(mux1, 'y', 2, mux2, 'y', 4)) {
-        cout << "Bidirectional connection between MUX1.y[2] and MUX2.y[4]" << endl;
-    } else {
-        cout << "No bidirectional connection between MUX1.y[2] and MUX2.y[4]" << endl;
-    }
+    cout << "Breadboard2 connections:\n";
+    breadboard2.printConnections();
+    cout << "\n";
+
+    cout << "Checking bidirectional connection between mux1.y[0] and breadboard1.pin[10]: ";
+    cout << (checkBidirectionalConnection(mux1, 'y', 0, breadboard1, 'p', 10) ? "true" : "false") << endl;
 
 
     return 0;
